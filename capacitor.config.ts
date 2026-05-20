4f763f77af45f1ff84b92896307124b2f642aead
Gnanendra494 <gnanendragnana143@gmail.com>
2026-05-20 23:55:20 +0530

    Initial commit of DocVault project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.docvault.app',
+  appName: 'DocVault',
+  webDir: 'dist'
+};
+
+export default config;
